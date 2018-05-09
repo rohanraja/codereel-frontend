91e4a1766689824782dea24c3171028d6a37fb89
Rohan Raja <rohan@Rohans-MacBook-Pro.local>
2018-05-10 00:31:02 +0530

    Added all tests for codewalk

--- src/components/controlpanel/tests/codewalkActions.test.js
+++ src/components/controlpanel/tests/codewalkActions.test.js
@@ -2,14 +2,47 @@ import * as actions from '../actions'
 import * as types from '../../../store/types'
 
 
+
 describe('Debug_next_called', () => {
   it('should increment line of current file when more lines available', () => {
 
+    testIncrementor(0, types.INCREMENT_ACTIVE_LINE, 0);
+  })
+  it('should increment fileRun when no more lines available', () => {
+
+    testIncrementor(2, types.INCREMENT_ACTIVE_FILERUN, 0);
+  })
+  
+  it('should do nothing when debug end reached', () => {
+
+    testIncrementor(1, "", 2);
+  })
+})
+
+
+describe('Debug_Prev_called', () => {
+  it('should decrement line of current file when not present at start line pos', () => {
+
+    testDecrementor(2, types.DECREMENT_ACTIVE_LINE, 0);
+  })
+  it('should decrement fileRun when present at start line pos', () => {
+
+    testDecrementor(0, types.DECREMENT_ACTIVE_FILERUN, 1);
+  })
+
+  it('should do nothing when debug beginning reached', () => {
+
+    testDecrementor(0, "", 0);
+  })
+})
+
+function testIncrementor(lineSeq, expActn, fileRunIdx)
+{
    const getState = () => (
     {
         activeFrame: {
-          fileRunIdx: 0,
-          lineSeqIdx: 1,
+          fileRunIdx: fileRunIdx,
+          lineSeqIdx: lineSeq,
           maxFileRuns: 3,
           maxLineSeqs: [3,5,2]
         },
@@ -20,9 +53,42 @@ describe('Debug_next_called', () => {
 
     actions.nextCalled()(dispatch, getState);
 
+  if(expActn != "")
+  {
     expect(dispatch.mock.calls.length).toBe(1);
-    expect(dispatch.mock.calls[0][0].type).toBe(types.INCREMENT_ACTIVE_LINE);
+    expect(dispatch.mock.calls[0][0].type).toBe(expActn);
+  }
+  else
+  {
+    expect(dispatch.mock.calls.length).toBe(0);
+  }
+}
 
+function testDecrementor(lineSeq, expActn, fileRunIdx)
+{
+   const getState = () => (
+    {
+        activeFrame: {
+          fileRunIdx: fileRunIdx,
+          lineSeqIdx: lineSeq,
+          maxFileRuns: 3,
+          maxLineSeqs: [3,5,2]
+        },
+    }
+  );
 
-  })
-})
+    const dispatch = jest.fn();
+
+    actions.prevCalled()(dispatch, getState);
+
+  if(expActn != "")
+  {
+    expect(dispatch.mock.calls.length).toBe(1);
+    expect(dispatch.mock.calls[0][0].type).toBe(expActn);
+  }
+  else
+  {
+    expect(dispatch.mock.calls.length).toBe(0);
+  }
+}
+
