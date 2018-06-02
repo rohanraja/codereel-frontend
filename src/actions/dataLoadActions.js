cb2dff8d137e8eddf003979fbb636f455cccb0e7
Rohan Raja <rohan@Rohans-MacBook-Pro.local>
2018-06-02 17:55:11 +0530

    Refactored data loading method for api call

--- src/actions/dataLoadActions.js
+++ src/actions/dataLoadActions.js
@@ -1,38 +1,33 @@
 import * as types from 'store/types'
 let axios = require('axios');
 
+const API_URL = "http://localhost:3000/api/"
+
 export function fetchCodeWalkData() {
   return function (dispatch, getState) {
-    const state = getState();
 
-    const codeFilesUrl = "http://localhost:3000/api/codefiles";
-    const fileRunsUrl = "http://localhost:3000/api/fileruns";
-
-    axios.get(codeFilesUrl).then(
-        (response) => {
-            dispatch({
-              type: types.CODEFILES_RECIEVED,
-              payload: response.data
-            });
-        },
-        (err) => {
-            console.log(err);
-        }
-    );
-
-    axios.get(fileRunsUrl).then(
-        (response) => {
-            dispatch({
-              type: types.FILERUNS_RECIEVED,
-              payload: response.data
-            });
-        },
-        (err) => {
-            console.log(err);
-        }
-    );
+    loadDataForStateKey(dispatch, "codefiles", types.CODEFILES_RECIEVED);
+    loadDataForStateKey(dispatch, "fileruns", types.FILERUNS_RECIEVED);
 
   };
-
 }
+
+function loadDataForStateKey(dispatch, keyName, actionType)
+{
+    const Url = API_URL + keyName;
+
+    axios.get(Url).then(
+        (response) => {
+            dispatch({
+              type: actionType,
+              payload: response.data
+            });
+        },
+        (err) => {
+            console.log(err);
+        }
+    );
+}
+
+
 // -- new_actionCreator_hook --
