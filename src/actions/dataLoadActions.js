0be32cff33b7e3a2e5f6c4ffb845ffcbd1d491f2
Rohan Raja <rohan@Rohans-MacBook-Pro.local>
2018-06-02 17:59:08 +0530

    Added codestory api call to dataload action

--- src/actions/dataLoadActions.js
+++ src/actions/dataLoadActions.js
@@ -8,6 +8,7 @@ export function fetchCodeWalkData() {
 
     loadDataForStateKey(dispatch, "codefiles", types.CODEFILES_RECIEVED);
     loadDataForStateKey(dispatch, "fileruns", types.FILERUNS_RECIEVED);
+    loadDataForStateKey(dispatch, "codeStory", types.CODESTORY_RECIEVED);
 
   };
 }
--- src/store/reducers/codeStoryReducer.js
+++ src/store/reducers/codeStoryReducer.js
@@ -5,6 +5,11 @@ import * as types from '../types'
 export function codeStoryReducer(state = initialState.codeStory, action) {
   switch (action.type) {
 
+    case types.CODESTORY_RECIEVED:
+      if(action.payload == undefined ||  Object.keys(action.payload).length == 0)
+        return initialState.codeStory;
+      return action.payload;
+
     default:
       return state
   }
